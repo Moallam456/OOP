d519de38343138ec4eb255a2d134f6576b8398d5
Kareem Hussein <kareemmhusamit@gmail.com>
2025-08-22 20:23:52 +0300

    V1

---
+++ f1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Apllication_1 ... test_1");
+	return 0;
+}
---
+++ f3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Apllication_3 ... test_1");
+	return 0;
+}
