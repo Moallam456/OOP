3d1ebffd0111921642c0d0c344f9a3a98ff68683
Moallam456 <137609526+Moallam456@users.noreply.github.com>
2025-09-15 17:06:07 +0300

    Testlast

---
+++ module.h
@@ -0,0 +1,14 @@
+#ifndef module_h
+#define module_h
+#define nameSize 20
+#define studentNumber 5
+typedef struct
+{
+    char name[nameSize];
+    int age;
+    int degree;
+    int section;
+}studentBase;
+
+
+#endif // module_h
